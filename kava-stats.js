ca1f32f73c1a386b3570edcd580308a74df4b8f0
adamrturman <adamrturman@gmail.com>
2021-09-09 13:07:13 -0500

    remove log

--- kava-stats.js
+++ kava-stats.js
@@ -930,8 +930,6 @@ const updateDisplayValues = async (denoms) => {
   const defiCoinsSupply = await mapSupplyAndMarket(denoms, siteData)
   siteData['defiCoinsSupply'] = defiCoinsSupply;
 
-  console.log(siteData)
-
   // set display values
   await setTotalEarningsDisplayValues(denoms, siteData, cssIds)
 
